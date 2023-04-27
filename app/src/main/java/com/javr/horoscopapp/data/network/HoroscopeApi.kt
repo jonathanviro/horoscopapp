fd95b010f04358f8c5534117ec95d3618e499c89
Jonathan Vivero <jonathanviro37@gmail.com>
2023-04-26 23:50:36 -0500

    implemented Retrofit

---
+++ app/src/main/java/com/javr/horoscopapp/data/network/HoroscopeApi.kt
@@ -0,0 +1,15 @@
+package com.javr.horoscopapp.data.network
+
+import com.javr.horoscopapp.data.network.model.HoroscopeResponse
+import retrofit2.Response
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface HoroscopeApi {
+
+    @POST(".")
+    suspend fun getHoroscope(
+        @Query("sign") sign: String,
+        @Query("day") day: String
+    ): Response<HoroscopeResponse>
+}
