68951f2f146cc35440c0636062d079e609d9647c
Jonathan Vivero <jonathanviro37@gmail.com>
2023-04-26 22:52:21 -0500

    implemented Dagger Hilt

---
+++ app/src/main/java/com/javr/horoscopapp/ui/HoroscopApp.kt
@@ -0,0 +1,7 @@
+package com.javr.horoscopapp.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HoroscopApp: Application()
---
+++ app/src/main/java/com/javr/horoscopapp/ui/list/ListViewModel.kt
@@ -0,0 +1,9 @@
+package com.javr.horoscopapp.ui.list
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ListViewModel @Inject constructor(): ViewModel(){
+}
