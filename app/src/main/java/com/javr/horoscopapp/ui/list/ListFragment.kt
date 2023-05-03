28e36e7e9611168ff4c5bc37b28f3547486ffe90
Jonathan Vivero <jonathanviro37@gmail.com>
2023-05-02 22:50:47 -0500

    Created DetailActivity

--- app/src/main/java/com/javr/horoscopapp/ui/list/ListFragment.kt
+++ app/src/main/java/com/javr/horoscopapp/ui/list/ListFragment.kt
@@ -7,17 +7,36 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.javr.horoscopapp.R
+import com.javr.horoscopapp.databinding.FragmentListBinding
+import com.javr.horoscopapp.databinding.FragmentLuckyBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ListFragment : Fragment() {
 
     private val viewModel by viewModels<ListViewModel>()
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnAries.setOnClickListener {
+            //abrirDetalle
+        }
+
+        binding.btnCapricornio.setOnClickListener {
+            //abrirDetalle
+        }
+
+        binding.btnLeo.setOnClickListener {
+            //abrirDetalle
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
